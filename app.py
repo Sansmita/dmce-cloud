5c5d4767df9c6982334810b5706ac42ca75c6b53
Sansmita <113035059+Sansmita@users.noreply.github.com>
2024-01-12 15:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Web App with Python Flask!'
-
-app.run(host='0.0.0.0', port=81)
+def hello_world():
+    return 'hello,world! This is a flask application.'
+ if _name_ == '__main__':   
+                     
+app.run(host='0.0.0.0', port=5000)
